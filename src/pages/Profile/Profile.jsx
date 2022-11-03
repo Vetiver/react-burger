85a221947b723df9202462d07c14c7797ca1f428
Vetiver <dmitriy_serga@mail.ru>
2022-11-03 12:26:40 +0300

    комит

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -16,11 +16,15 @@ import NavBar from "../../components/NavBar/NavBar.jsx";
 function Profile(props) {
   const userInfo = useSelector((state) => state.profileReducer.userInfoData);
   const [form, setValue] = useState({ email: "", name: "", password: "" });
-  const [disabled, setDisabled] = useState({ disabled: true})
+  const [disabled, setDisabled] = useState({ disabled: true });
   let dispatch = useDispatch();
   const onInputChange = (e) => {
     setValue({ ...form, [e.target.name]: e.target.value });
-    if(userInfo.email == form.email && userInfo.name == form.name && form.password == '') {
+    if (
+      userInfo.email == form.email &&
+      userInfo.name == form.name &&
+      form.password == ""
+    ) {
       setDisabled(false);
     }
   };
@@ -60,15 +64,14 @@ function Profile(props) {
       setValue({
         name: form.name,
         email: form.email,
-      })
+      });
       setDisabled(true);
     },
     [form]
   );
 
   return (
-    !!userInfo &&
-    (disabled ? (
+    !!userInfo && (
       <div className={`${Style.mainContainer}`}>
         <div>
           <NavBar />
@@ -108,11 +111,11 @@ function Profile(props) {
           />
 
           <div className={`${Style.buttonsContainer}`}>
-            <Button disabled={true} type="secondary" size="small">
+            <Button disabled={disabled} type="secondary" size="small">
               <p className={`text text_type_main-default`}>Отмена</p>
             </Button>
             <Button
-              disabled={true}
+              disabled={disabled}
               onClick={updateUserData}
               type="primary"
               size="small"
@@ -122,56 +125,7 @@ function Profile(props) {
           </div>
         </div>
       </div>
-    ) : (
-      <div className={`${Style.mainContainer}`}>
-        <div>
-          <NavBar />
-          <p
-            className={`${Style.text} text text_type_main-default text_color_inactive`}
-          >
-            В этом разделе вы можете изменить свои персональные данные
-          </p>
-        </div>
-        <div className={`${Style.inputContainer}`}>
-          <Input
-            type={"text"}
-            placeholder={"Имя"}
-            onChange={onInputChange}
-            name={"name"}
-            value={form.name}
-            error={false}
-            errorText={"Ошибка"}
-            size={"default"}
-          />
-          <EmailInput
-            type={"email"}
-            placeholder={"E-mail"}
-            onChange={onInputChange}
-            value={form.email}
-            error={false}
-            name={"email"}
-          />
-          <Input
-            type={"password"}
-            placeholder={"Пароль"}
-            icon={"ShowIcon"}
-            name={"password"}
-            onChange={onInputChange}
-            value={form.password}
-            error={false}
-          />
-
-          <div className={`${Style.buttonsContainer}`}>
-            <Button onClick={revoke} type="secondary" size="small">
-              <p className={`text text_type_main-default`}>Отмена</p>
-            </Button>
-            <Button onClick={updateUserData} type="primary" size="small">
-              <p className={`text text_type_main-default`}>Сохранить</p>
-            </Button>
-          </div>
-        </div>
-      </div>
-    ))
+    )
   );
 }
 
